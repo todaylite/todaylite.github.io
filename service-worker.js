e59fdf3453373e20503986c42d11cde75b9384d2
todaylite <todayliteconnect@gmail.com>
2025-08-09 13:19:43 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon || '/icon.png',
+      badge: data.badge || '/badge.png',
+    })
+  );
+});
